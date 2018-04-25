6780ed9d703cdc9dfde2c6a33b6a002eab01598f
leoiphonedev <aggarwalaman1985@gmail.com>
2018-04-25 20:59:24 +0530

    Initial Commit

---
+++ ReusableComponent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReusableComponent
+//
+//  Created by Aman Aggarwal on 25/04/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
